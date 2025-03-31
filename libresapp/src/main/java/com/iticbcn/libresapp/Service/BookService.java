6bd34d7864f7e00daba9730b39130b89c01f5f9d
Seth <sethgzbr@gmail.com>
2025-03-31 21:58:13 +0200

    service acabat

---
+++ libresapp/src/main/java/com/iticbcn/libresapp/Service/BookService.java
@@ -0,0 +1,13 @@
+package com.iticbcn.libresapp.Service;
+import java.util.Optional;
+import java.util.Set;
+import com.iticbcn.libresapp.Model.Llibre;
+
+public interface BookService {
+
+    Set<Llibre> findAllLlibres();
+    Llibre findByTitol(String titol);
+    Set<Llibre> findByTitolAndEditorial(String titol, String editorial);
+    Optional<Llibre> findByIdLlibre(Long id);
+    boolean validateIsbnFormat(String isbn);
+}
---
+++ libresapp/src/main/java/com/iticbcn/libresapp/Service/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.iticbcn.libresapp.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iticbcn.libresapp.Model.Llibre;
+import com.iticbcn.libresapp.Repositoris.BookRepository;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Set<Llibre> findAllLlibres() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Llibre findByTitol(String titol) {
+        return bookRepository.findByTitol(titol);
+    }
+
+    @Override
+    public Set<Llibre> findByTitolAndEditorial(String titol, String editorial) {
+        return (Set<Llibre>) bookRepository.findByTitolAndEditorial(titol, editorial);
+    }
+
+    @Override
+    public Optional<Llibre> findByIdLlibre(Long id) {
+        return (Optional <Llibre>) bookRepository.findByIdLlibre(id);
+    }
+
+    @Override
+    public boolean validateIsbnFormat(String isbn) {
+        if (isbn == null || (isbn.length() != 10 && isbn.length() != 13)) {
+            return false;
+        }
+        return isbn.matches("\\d+");
+    }
+}
